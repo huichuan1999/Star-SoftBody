f5d4a2637db7306079db1edba5a2450e02cfe73d
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-07-23 00:58:44 +0100

    add mouse Interact

--- sketch.js
+++ sketch.js
@@ -2,7 +2,8 @@ let physics;
 let tailPhysics;
 let stars = [];
 let angStars = [];
-let numStars = 2;
+let numStars = 0;
+let particleGrabRadius = 10;
 
 function setup() {
   createCanvas(600,600);
@@ -32,12 +33,79 @@ function draw() {
   
 }
 
+let lockedParticle = null; // To store the particle that is being dragged
+
 function mousePressed() {
+  // Check each star
+  for (let star of stars) {
+    // Check each point in the star
+    for (let point of star.points) {
+      let d = dist(mouseX, mouseY, point.x, point.y);
+      if (d < particleGrabRadius) {
+        lockedParticle = point; // Store the particle that is being dragged
+        lockedParticle.lock(); // Lock the particle
+        break;
+      }
+    }
+
+    // If a particle has been found and locked, break the outer loop as well
+    if (lockedParticle != null) {
+      break;
+    }
+  }
+
+  // If a particle is found and locked, then no need to add new repulsion behavior
+  if (lockedParticle != null) {
+    return;
+  }
+
+  // Else, add repulsion behavior as before
   let centerX = mouseX;
   let centerY = mouseY;
   let star = new Star(centerX, centerY, random(3,7), random(10, 20), random(30, 50));
   stars.push(star);
 
-  // let repulsion = new AttractionBehavior(new Vec2D(mouseX, mouseY), 100, -0.01);
-  // physics.addBehavior(repulsion);
+  let repulsion = new AttractionBehavior(new Vec2D(mouseX, mouseY), 100, -0.01);
+  physics.addBehavior(repulsion);
 }
+
+function mouseDragged() {
+  // If a particle is being dragged, update its position
+  if (lockedParticle != null) {
+    lockedParticle.set(mouseX, mouseY);
+  }
+}
+
+function mouseReleased() {
+  // If a particle is being dragged, unlock it when the mouse is released
+  if (lockedParticle != null) {
+    lockedParticle.unlock();
+    lockedParticle = null; // Clear the stored particle
+  }
+}
+
+
+// function mousePressed() {
+
+//    // Check each star
+//    for (let star of stars) {
+//     // Check each point in the star
+//     for (let point of star.points) {
+//       let d = dist(mouseX, mouseY, point.x, point.y);
+//       if (d < particleGrabRadius) {
+//         lockedParticle = point; // Store the particle that is being dragged
+//         lockedParticle.lock(); // Lock the particle
+//         break;
+//       }
+//     }
+//   }
+
+//   //add stars
+//   // let centerX = mouseX;
+//   // let centerY = mouseY;
+//   // let star = new Star(centerX, centerY, random(3,7), random(10, 20), random(30, 50));
+//   // stars.push(star);
+
+//   // let repulsion = new AttractionBehavior(new Vec2D(mouseX, mouseY), 100, -0.01);
+//   // physics.addBehavior(repulsion);
+// }
